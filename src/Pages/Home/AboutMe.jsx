83351b3bdfc04a35bc58130a94f1abd1d31cd895
saidhanya184 <saidhanyashree@gmail.com>
2023-11-09 18:19:17 +0530

    first commit

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,22 @@
+export default function AboutMe() {
+  return (
+    <section id="AboutMe" className="about--section">
+      <div className="about--section--img">
+        <img src="./img/female-developer.jpg" alt="About Me" />
+      </div>
+      <div className="hero--section--content--box about--section--box">
+        <div className="hero--section--content">
+          <p className="section--title">About</p>
+          <h1 className="skills-section--heading">About Me</h1>
+          <p className="hero--section-description">
+            I am fluent in Front-end Web Development and I am still learning back-end
+            development. I also Like Android Development and I have worked on React-js.
+          </p>
+          <p className="hero--section-description">
+            I am looking forward to create many more frontend and backend projects.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
